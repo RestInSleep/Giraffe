436c03186d03eae9fb361e09535f1ee24ddf4920
Jan <jasjago@gmail.com>
2022-09-25 23:49:00 +0200

    dodanie

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+             const decimal priceForSqF=20;
+             
+             Console.WriteLine("Enter width (m)");
+             decimal a = decimal.Parse(Console.ReadLine());
+
+             Console.WriteLine("Enter length (m)");
+             decimal b = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The price is {a*b*priceForSqF}");
+
+            }
+
+
+
+
+
+
+
+    }
+}
